11e242150c96a4ce5bb2d3d86c1b06458f7ca8cb
Jayanta66 <jayhalder18@gmail.com>
2025-02-01 15:07:58 +0530

    add

--- src/test/java/com/taskmanagement/taskmanagement/TaskRepositoryTest.java
+++ src/test/java/com/taskmanagement/taskmanagement/TaskRepositoryTest.java
@@ -22,64 +22,64 @@ import com.taskmanagement.taskmanagement.repository.TaskRepository;
 @Rollback(false)
 public class TaskRepositoryTest {
 	
-	@Autowired
-	private TestEntityManager entityManager;
-
-	@Autowired	
-	private TaskRepository taskRepository;
-	
-	@Test
-	public void testCreateTask() {
-		Date createdAt = new Date(); 
-		Date updatedAt = new Date();
-		Task task = new Task("Java Full Web Development","Java Develper","Completed",createdAt,updatedAt);	
-		Task savedtask = taskRepository.save(task);
-		System.out.println(task);
-		assertThat(savedtask.getId()).isGreaterThan(0);
-		
-	}
-	
-
-	@Test
-	public void testListAllTask() {
-		Iterable<Task> listTasks = taskRepository.findAll();
-		listTasks.forEach(task -> System.out.println(task));
-	}
-	
-	@Test
-	public void testGetTaskById() {
-		Task task = taskRepository.findById((long) 42).get();
-		System.out.println(task);
-		assertThat(task).isNotNull();
-	}
-	
-	@Test
-	public void testUpdateTaskDetails() {
-		Task task = taskRepository.findById((long) 42).get();
-		task.setId((long)42);
-		task.setTitle("Jayanta Halderrrrrrrrrrrrrrrrrrrrr");
-		task.setDescription("dsfdsf");
-		task.setStatus("Completed");		
-		System.out.println(taskRepository.save(task));
-
-	}
-	
-	@Test
-	public void testDeleteTask() {
-		Long taskId = (long) 44;
-		taskRepository.deleteById(taskId);
-		System.out.println("Deleted Successfully");
-
-		
-	}
-	
-	@Test
-	public void testGetByTaskTitle() {
-		String title = "Full Stack Java";
-		Task task = taskRepository.getTitle(title);
-		System.out.println(task);
-		assertThat(task).isNotNull();
-	}
+//	@Autowired
+//	private TestEntityManager entityManager;
+//
+//	@Autowired	
+//	private TaskRepository taskRepository;
+//	
+//	@Test
+//	public void testCreateTask() {
+//		Date createdAt = new Date(); 
+//		Date updatedAt = new Date();
+//		Task task = new Task("Java Full Web Development","Java Develper","Completed",createdAt,updatedAt);	
+//		Task savedtask = taskRepository.save(task);
+//		System.out.println(task);
+//		assertThat(savedtask.getId()).isGreaterThan(0);
+//		
+//	}
+//	
+//
+//	@Test
+//	public void testListAllTask() {
+//		Iterable<Task> listTasks = taskRepository.findAll();
+//		listTasks.forEach(task -> System.out.println(task));
+//	}
+//	
+//	@Test
+//	public void testGetTaskById() {
+//		Task task = taskRepository.findById((long) 42).get();
+//		System.out.println(task);
+//		assertThat(task).isNotNull();
+//	}
+//	
+//	@Test
+//	public void testUpdateTaskDetails() {
+//		Task task = taskRepository.findById((long) 42).get();
+//		task.setId((long)42);
+//		task.setTitle("Jayanta Halderrrrrrrrrrrrrrrrrrrrr");
+//		task.setDescription("dsfdsf");
+//		task.setStatus("Completed");		
+//		System.out.println(taskRepository.save(task));
+//
+//	}
+//	
+//	@Test
+//	public void testDeleteTask() {
+//		Long taskId = (long) 44;
+//		taskRepository.deleteById(taskId);
+//		System.out.println("Deleted Successfully");
+//
+//		
+//	}
+//	
+//	@Test
+//	public void testGetByTaskTitle() {
+//		String title = "Full Stack Java";
+//		Task task = taskRepository.getTitle(title);
+//		System.out.println(task);
+//		assertThat(task).isNotNull();
+//	}
 
 
 }
