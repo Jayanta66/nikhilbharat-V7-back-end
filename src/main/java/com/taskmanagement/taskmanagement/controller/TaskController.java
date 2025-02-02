06645de0747f8e56dad1a36ffad9b5d02053f0cd
Jayanta66 <jayhalder18@gmail.com>
2025-02-02 11:17:13 +0530

    add

--- src/main/java/com/taskmanagement/taskmanagement/controller/TaskController.java
+++ src/main/java/com/taskmanagement/taskmanagement/controller/TaskController.java
@@ -22,8 +22,7 @@ import com.taskmanagement.taskmanagement.model.Task;
 import com.taskmanagement.taskmanagement.repository.TaskRepository;
 
 @RestController
-//@CrossOrigin("http://localhost:3000")
-@CrossOrigin("https://jayanta66.github.io/nikhilbharat-V7-front-end:3000")
+@CrossOrigin("https://jayanta66.github.io/nikhilbharat-V7-front-end")
 public class TaskController {
 
 	@Autowired
